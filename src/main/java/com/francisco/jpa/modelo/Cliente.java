03ef0aa18128b28d18531bc642aa8bdf1ec7599f
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-02-25 09:21:46 -0300

    Relacionamento Um para Um

--- src/main/java/com/francisco/jpa/modelo/Cliente.java
+++ src/main/java/com/francisco/jpa/modelo/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Cliente {
@@ -17,6 +18,17 @@ public class Cliente {
 	private String profissao;
 	
 	private String endereco;
+	
+	@OneToOne
+	private Conta conta;
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
 
 	public Long getId() {
 		return id;
