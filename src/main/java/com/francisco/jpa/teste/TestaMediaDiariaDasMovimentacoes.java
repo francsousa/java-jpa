4223f18976d0648c440df9a3f465ccf2af465695
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-03-15 14:35:18 -0300

    Organizando o código

--- src/main/java/com/francisco/jpa/teste/TestaMediaDiariaDasMovimentacoes.java
+++ src/main/java/com/francisco/jpa/teste/TestaMediaDiariaDasMovimentacoes.java
@@ -2,24 +2,14 @@ package com.francisco.jpa.teste;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-
 import com.francisco.jpa.modelo.MediaComData;
+import com.francisco.jpa.modelo.dao.MovimentacaoDao;
 
 public class TestaMediaDiariaDasMovimentacoes {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-		EntityManager em = emf.createEntityManager();
-
-		String jpql = "select new com.francisco.jpa.modelo.MediaComData(avg(m.valor), day(m.data), month(m.data)) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
-
-		TypedQuery<MediaComData> query = em.createQuery(jpql, MediaComData.class);
-		List<MediaComData> somaDasMovimentacoes = query.getResultList();
+		List<MediaComData> somaDasMovimentacoes = new MovimentacaoDao().getMediaDiariaDasMovimentacoes();
 		
 		for (MediaComData resultado : somaDasMovimentacoes) {
 			System.out.println("A média das movimentações do dia " + resultado.getDia() + "/" + resultado.getMes() + " é: " + resultado.getValor());
